12240843016108b2050611834bff7f857a5c8d23
Adrian Lopez <adrian.08lv@gmail.com>
2022-05-10 15:07:07 -0300

    Detalles de vista de listajsx

--- src/components/Lista.jsx
+++ src/components/Lista.jsx
@@ -14,7 +14,8 @@ export default class Lista extends Component {
                   <li className='user-container' key={x.id}>
                       <p className='user-icon'></p>
                       <div className='data-container'>
-                        <p className='user-name'>{x.name}</p><p>{x.email}</p>
+                        <p className='user-name'>{x.name}</p>
+                        <p>{x.email}</p>
                       </div>
                       <p onClick={this.handleClick(x.id)} className="edit-button">Editar</p>
                   </li>)} 
